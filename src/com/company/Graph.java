9f38ccc9af1bdbc748489b819b35d8d31b6967f5
jonathanbain1992 <2092528b@student.gla.ac.uk>
2017-11-09 22:33:13 +0000

    Added matrix-generator functionality with symmetry check, general code tidying, added time-cost measurements to the system out.

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,45 +1,58 @@
 package com.company;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 
 public class Graph {
 
     private ArrayList<Vertex> globalNeighbourhood;
 
-    public Graph(){
+    public Graph() {
         this.globalNeighbourhood = new ArrayList<Vertex>();
     }
 
-    public boolean addVertex(Vertex v){
+    public boolean addVertex(Vertex v) {
         return globalNeighbourhood.add(v);
     }
 
-    public boolean removeVertex(Vertex v){
+    public boolean removeVertex(Vertex v) {
         return globalNeighbourhood.remove(v);
     }
 
-    public boolean clearNeighbourhood(){
+    public boolean clearNeighbourhood() {
         globalNeighbourhood.clear();
         return true;
     }
 
-    public int getNeighbourhoodSize(){
+    public int getNeighbourhoodSize() {
         return globalNeighbourhood.size();
     }
 
-    public ArrayList<Vertex> getNeighbourhood(){
+    public ArrayList<Vertex> getNeighbourhood() {
         return this.globalNeighbourhood;
     }
 
-    public boolean populateGraph(int size){
-        for(int i=0; i<size; i++){
+    public boolean populateGraph(int size) {
+        long time = System.nanoTime();
+        for (int i = 0; i < size; i++) {
             globalNeighbourhood.add(new Vertex());
         }
+        System.out.println("Graph Population Cost"+(System.nanoTime()-time));
         return true;
     }
-    public boolean setNeighbourRelations(double probability, Vertex a, Vertex b){
-        if(!a.equals(b)) {
+    public boolean setNeighbourhood(double probability) {
+        long time = System.nanoTime();
+        for (Vertex v : globalNeighbourhood) {
+            for (Vertex v2 : globalNeighbourhood) {
+                setNeighbourRelations(probability, v, v2);
+            }
+        }
+        System.out.println("Generating Neighbourhood Of Entire Graph Cost: "+ (System.nanoTime()-time));
+        return true;
+    }
+
+    public boolean setNeighbourRelations(double probability, Vertex a, Vertex b) {
+        if (!a.equals(b)) {
             if (Math.random() < probability) {
                 if (!(a.containsNeighbour(b) || b.containsNeighbour(a))) {
                     //passes to vertex method which itself returns a boolean.
@@ -52,77 +65,97 @@ public class Graph {
         return false;
     }
 
-
-    public int[][] generateMatrix(){
-        int[][] returnArr = new int[(int)Math.pow(globalNeighbourhood.size(), 2)][(int)Math.pow(globalNeighbourhood.size(), 2)];
+    public int[][] generateMatrix() {
+        long time = System.nanoTime();
+        int[][] returnArr = new int[(int) Math.pow(globalNeighbourhood.size(), 2)][(int) Math.pow(globalNeighbourhood.size(), 2)];
 
         int i = 0;
-        for (Vertex v1: globalNeighbourhood){
-            for (Vertex v2: globalNeighbourhood){
-                if (v1.containsNeighbour(v2)){
-                    returnArr[i/globalNeighbourhood.size()][i%globalNeighbourhood.size()] = 1;
+        for (Vertex v1 : globalNeighbourhood) {
+            for (Vertex v2 : globalNeighbourhood) {
+                if (v1.containsNeighbour(v2)) {
+                    returnArr[i / globalNeighbourhood.size()][i % globalNeighbourhood.size()] = 1;
                 }
                 i++;
             }
         }
 
+        System.out.println("Generating Matrix Version Cost: "+(System.nanoTime()-time));
+
         return returnArr;
 
     }
 
-    public void printMatrix(){
-        int[][] matrix = generateMatrix();
-        for (int i= 0; i < Math.sqrt(matrix.length); i++){
-            for (int j = 0; j < Math.sqrt(matrix.length); j++) {
-                if (i==0 && j==0 || i==Math.sqrt(matrix.length)-1 && j==0){
-                    System.out.print("[ ");
-                } else if (i!=0 && j==0){
-                    System.out.print("| ");
-                }
+    public int[][] transpose(int[][] matrix){
+        long time = System.nanoTime();
+        int dimension = globalNeighbourhood.size();
+        int[][] flipped = new int[dimension][dimension];
 
-                System.out.print(matrix[i][j]);
-
-                if (i==0 && j==Math.sqrt(matrix.length)-1 || i== Math.sqrt(matrix.length)-1 &&j==Math.sqrt(matrix.length)-1){
-                    System.out.print(" ]");
-                } else if (i!=0 && j==Math.sqrt(matrix.length)-1){
-                    System.out.print(" |");
-                }
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                flipped[i][j] = matrix[j][i];
             }
-            System.out.print("\n");
         }
-        if (isMatrixDiagonal()){
-            System.out.println("Matrix is diagonal.");
-        }
+       // System.out.println("Matrix Transposing Cost: "+(System.nanoTime()-time));
+        return flipped;
     }
 
-    public boolean isMatrixDiagonal(){
-        int[][] matrix = generateMatrix();
-        int[][] flipped = transpose(matrix);
+    //verification that the output is consistently formatted when rendered as matrix
 
-        for (int i = 0; i < globalNeighbourhood.size(); i++){
-            for (int j=0; j< globalNeighbourhood.size(); j++){
-                if (matrix[i][j] != flipped[i][j]){
-                    return false;
+
+    public boolean isMatrixDiagonal(int[][] matrix) throws Exception {
+
+        long time = System.nanoTime();
+
+        for (int i = 0; i < globalNeighbourhood.size(); i++) {
+            for (int j = 0; j < globalNeighbourhood.size(); j++) {
+
+                if (matrix[i][j] != transpose(matrix)[i][j]) {
+                    throw new Exception("Matrix was invalid when tested for its diagonal");
                 }
             }
         }
+
+        System.out.println("Checking Matrix Symmetric Cost: "+(System.nanoTime()-time));
 
         return true;
 
     }
 
 
+    public void printMatrix() {
 
-    public int[][] transpose(int[][] matrix){
-        int dimension = globalNeighbourhood.size();
-        int[][] flipped = new int[dimension][dimension];
+        try {
+            long time = System.nanoTime();
+            int[][] matrix = generateMatrix();
+            for (int i = 0; i < Math.sqrt(matrix.length); i++) {
+                for (int j = 0; j < Math.sqrt(matrix.length); j++) {
+                    if (i == 0 && j == 0 || i == Math.sqrt(matrix.length) - 1 && j == 0) {
+                        System.out.print("[ ");
+                    } else if (i != 0 && j == 0) {
+                        System.out.print("| ");
+                    }
 
-        for (int i=0; i < dimension; i++){
-            for (int j=0; j< dimension; j++){
-                flipped[i][j] = matrix[j][i];
+                    System.out.print(matrix[i][j]);
+
+                    if (i == 0 && j == Math.sqrt(matrix.length) - 1 || i == Math.sqrt(matrix.length) - 1 && j == Math.sqrt(matrix.length) - 1) {
+                        System.out.print(" ]");
+                    } else if (i != 0 && j == Math.sqrt(matrix.length) - 1) {
+                        System.out.print(" |");
+                    }
+                }
+                System.out.print("\n");
             }
+            System.out.println("Printing Matrix To System Out Cost: "+(System.nanoTime()-time));
+            isMatrixDiagonal(matrix);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Stack traceback:");
+            System.out.println(e.getStackTrace());
         }
-        return flipped;
     }
 
+
+
+
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
         // create a graph, take user input for number of nodes to make, populate graph.
         System.out.println("Please enter a number of nodes to create in the graph.");
         Scanner sc = new Scanner(System.in);
+
         Graph g = new Graph();
         g.populateGraph(sc.nextInt());
-        for (Vertex v : g.getNeighbourhood()) {
-            for (Vertex v2 : g.getNeighbourhood()) {
-                g.setNeighbourRelations(0.3, v, v2);
-            }
-            for (Vertex vertex : g.getNeighbourhood()) {
-                System.out.println(vertex.getHashCode());
-                System.out.println(vertex.getNeighbourhoodSize());
-            }
-        }
+        g.setNeighbourhood(0.3);
 
         g.printMatrix();
     }
